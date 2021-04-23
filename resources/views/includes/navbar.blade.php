84cb7f48513dd672cf6ccf70ae8d70fa6b4ad6c7
Miguel Zapata <zpmaepp@gmail.com>
2021-04-22 22:09:40 -0500

    first commit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-dark bg-dark mb-2">
+  <div class="container-fluid">
+    <a class="navbar-brand mx-2" href="/">
+      <i class="bi bi-archive"></i>
+      Tasks App</a>
+
+
+    <div class="col-auto me-2">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="row mx-auto">
+      <div class="col-auto mx-auto">
+        <form class="d-flex mx-2">
+          <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar">
+          <button class="btn btn-success" type="submit">Buscar</button>
+        </form>
+      </div>
+      <div class="col-auto">
+        <a type="button" class="btn btn-primary" href="/add">añadir</a>
+      </div>
+    </div>
+
+  </div>
+</nav>
